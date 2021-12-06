8612f01a6cbda04d7fffdc083733207d8713ec01
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-06 17:39:10 +0530

    fix: pom.xml

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-06T15:09:38+0530",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.13 (Oracle Corporation)"
+    date = "2021-12-06T17:36:17+0530",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.13 (Oracle Corporation)"
 )
 @Component
 @Primary
