8db36c813852df1052da4e1bd9a31f29e5371b35
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-07 18:39:15 +0530

    fix: mysql dialect

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-06T17:36:17+0530",
+    date = "2021-12-07T18:35:51+0530",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.13 (Oracle Corporation)"
 )
 @Component
