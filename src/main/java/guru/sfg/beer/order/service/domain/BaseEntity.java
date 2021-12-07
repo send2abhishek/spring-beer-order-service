87a5aa710d2789297d7e2e0e4849812baede85f8
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-07 18:25:02 +0530

    fix: Beer Domain class

--- src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
@@ -47,12 +47,9 @@ public class BaseEntity {
 
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Type(type="org.hibernate.type.UUIDCharType")
-    @Column(length = 36, updatable = false, nullable = false )
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false,nullable = false)
+    @Type(type = "org.hibernate.type.UUIDCharType")
     private UUID id;
 
     @Version
